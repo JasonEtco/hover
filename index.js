43f8ff0372d3abe21b94481c86aa8fda7373f015
Jason <jasonetco@gmail.com>
2016-11-09 21:12:49 -0500

    :wrench: Fix for prod

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* eslint no-console: 0 */
 
-const app = require('express')();
+const express = require('express')();
+
+const app = express();
+
 const http = require('http').Server(app);
 const path = require('path');
 const webpack = require('webpack');
@@ -8,31 +11,37 @@ const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const config = require('./webpack.config');
-const prodConfig = require('./webpack.production.config');
 
-const c = process.env.NODE_ENV === 'production' ? prodConfig : config;
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 4000 : process.env.PORT;
 
-const compiler = webpack(c);
-const middleware = webpackMiddleware(compiler, {
-  publicPath: c.output.publicPath,
-  contentBase: 'public',
-  stats: {
-    colors: true,
-    hash: false,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    modules: false,
-  },
-});
+if (isDeveloping) {
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'public',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false,
+    },
+  });
 
-app.use(middleware);
-app.use(webpackHotMiddleware(compiler));
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
 
-app.get('*', (req, res) => {
-  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-  res.end();
-});
+  app.get('*', (req, res) => {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.end();
+  });
+} else {
+  app.use(express.static(path.join(__dirname, '/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
 
-const port = process.env.PORT || 4000;
 http.listen(port, () => console.log(`Running at http://localhost:${port}`));
