b8a5de4a17c8a6a6ab40e1d08eeb038eb3232120
Jason <jasonetco@gmail.com>
2016-11-10 13:29:45 -0500

    :point_up: Basic socket hover effect

--- app/main.js
+++ app/main.js
@@ -1,3 +1,16 @@
 import './styles.scss';
 
-console.log('Isn\'t this boilerplate wonderful?');
+const circles = document.querySelectorAll('.circle');
+
+circles.forEach((c, i) => {
+  c.addEventListener('mouseenter', () => socket.emit('hoverOn', i));
+  c.addEventListener('mouseleave', () => socket.emit('hoverOff', i));
+});
+
+socket.on('hoverOn', (i) => {
+  circles[i].classList.add('hover');
+});
+
+socket.on('hoverOff', (i) => {
+  circles[i].classList.remove('hover');
+});
