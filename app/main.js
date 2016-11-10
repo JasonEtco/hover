bcc34f3d7670d1cf42d4b0f73c17501c9349933d
JasonEtco <jasonetco@gmail.com>
2016-11-10 18:10:39 -0500

    :art: Fancy hover effects

--- app/main.js
+++ app/main.js
@@ -9,8 +9,12 @@ circles.forEach((c, i) => {
 
 socket.on('hoverOn', (i) => {
   circles[i].classList.add('hover');
+  if (document.querySelectorAll('.hover').length === circles.length) {
+    document.body.classList.add('all-active');
+  }
 });
 
 socket.on('hoverOff', (i) => {
   circles[i].classList.remove('hover');
+  document.body.classList.remove('all-active');
 });
